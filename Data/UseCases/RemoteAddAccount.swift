b66c3b638c25de1a0a23775d607ff4bdb1464eea
Bruno Russo <bruno.russo-c@adventhealth.com>
2021-05-11 11:43:52 -0300

    ensure RemoteAddAccount completes with error when HttpClient completes with invalid data

--- Data/UseCases/RemoteAddAccount.swift
+++ Data/UseCases/RemoteAddAccount.swift
@@ -16,6 +16,8 @@ public final class RemoteAddAccount {
             case .success(let data):
                 if let model: AccountModel = data.toModel() {
                     completion(.success(model))
+                } else {
+                    completion(.failure(.unexpected))
                 }
             case .failure: completion(.failure(.unexpected))
             }
